1a27b4a70e0e351dc41a7b3ac54fdf0c5c181e62
HollisDayCheer <hollisj@carleton.edu>
2016-03-31 19:02:26 -0700

    classwork

--- app/src/main/java/com/example/hollis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hollis/myapplication/MainActivity.java
@@ -1,35 +1,60 @@
 package com.example.hollis.myapplication;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    Robot mrRoboto;
+public class MainActivity extends AppCompatActivity implements FragmentRobot.RobotFightListener {
+    TextView textView;
+    FragmentRobot fragmentRobot1;
+    FragmentRobot fragmentRobot2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeViews();
-        intializeRobot();
-
+        initializeRobotFragment();
+        initializeRobotFragment2();
     }
 
-    public void initializeViews(){
-        TextView textView = findViewById(R.id.robot);
-        setRobotText();
-    };
 
-    public void initializeRobot(){
-        Robot mrRoboto = new Robot();
+
+    public void initializeRobotFragment(){
+        //instantiate a robotfragment
+        fragmentRobot1 = new FragmentRobot();
+
+        //Create our fragment manager to manage our fragments and transactions
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_1, fragmentRobot1);
+        fragmentRobot1.setPosition(1);
+        fragmentTransaction.commit();
+    }
+    public void initializeRobotFragment2(){
+        //instantiate a robotfragment
+        fragmentRobot2 = new FragmentRobot();
+
+        //Create our fragment manager to manage our fragments and transactions
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_2, fragmentRobot2);
+        fragmentRobot2.setPosition(2);
+        fragmentTransaction.commit();
     }
 
-    public void setRobotText(){
-        if(mrRoboto.isMurderer()){
-            textView.setText("Run!");
+    public void fightRobots(Robot robot, int position){
+        System.out.println(robot.getHealth() + " and position is: " + position);
+        if(position ==1){
+            fragmentRobot2.fightRobot(robot);
         }else{
-            textView.setText("Don't Run!");
+            fragmentRobot1.fightRobot(robot);
         }
+
+        //TODO: Make this function
+
     }
 }
